16349eaedfd229646850e86cc32dbc1e61b68ac1
fsww-btamila <73156456+fsww-btamila@users.noreply.github.com>
2021-02-09 12:52:10 +0530

    Specific ports are comment

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ const htmlPdfNode = require('html-pdf-node');
 });
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+//const PORT = process.env.PORT || 3000;
+app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
